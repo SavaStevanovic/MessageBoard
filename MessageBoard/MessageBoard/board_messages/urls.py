9825e74b719aec5b22942bc87a1b439018f831da
SavaStevanovic <sava.stevanovic@pmf.edu.rs>
2017-10-02 00:40:07 +0200

    Some basic templates have been added

--- MessageBoard/MessageBoard/board_messages/urls.py
+++ MessageBoard/MessageBoard/board_messages/urls.py
@@ -3,5 +3,12 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    # ex: /board_messages/
     url(r'^$', views.index, name='index'),
+    # ex: /board_messages/5/
+    url(r'^(?P<board_id>[0-9]+)/$', views.detail, name='detail'),
+    # ex: /board_messages/5/results/
+    url(r'^(?P<board_id>[0-9]+)/results/$', views.results, name='results'),
+    # ex: /board_messages/5/vote/
+    url(r'^(?P<board_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
--- MessageBoard/MessageBoard/board_messages/views.py
+++ MessageBoard/MessageBoard/board_messages/views.py
@@ -1,5 +1,22 @@
 from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
 
+from .models import Category, Board
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_boards_list = Board.objects.order_by('order')[:5]
+    context = {
+        'latest_boards_list': latest_boards_list,
+    }
+    return render(request, 'board_messages/index.html', context)
+
+def detail(request, board_id):
+    board = get_object_or_404(Board, pk=board_id)
+    return render(request, 'board_messages/detail.html', {'board': board})
+
+def results(request, board_id):
+    response = "You're looking at the results of board %s."
+    return HttpResponse(response % board_id)
+
+def vote(request, board_id):
+    return HttpResponse("You're voting on board %s." % board_id)
